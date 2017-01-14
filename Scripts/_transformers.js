49d544141fb04f9288c0e958b9fd781e1658ce37
Renju Koshy George01 <Renju_George01@ad.infosys.com>
2017-01-14 17:48:30 +1300

    Intial Load

---
+++ Scripts/_transformers.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+    $("#fullContainer").fullpage({
+        anchors: ['homeSection', 'eventsSection', 'speakersSection', 'aboutusSection', 'registerSection', 'contactSection'],
+        menu: '#transMenu',
+        //equivalent to jQuery `easeOutBack` extracted from http://matthewlein.com/ceaser/
+        easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)'
+    });
+
+    //EventManager Code
+    $().timelinr({
+        orientation: 'vertical',
+        issuesSpeed: 500,
+        datesSpeed: 300,
+        startAt: 3,
+        autoPlay: 'true'
+    });
+});
